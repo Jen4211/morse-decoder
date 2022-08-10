15864d402f392d04bc44f9d8a7aa18ddbb92f81c
Yauheni <jengulya4211@gmail.com>
2022-08-10 21:41:16 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -38,8 +38,21 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let sentence = '';
+    for (let i = 0; i < expr.length; i += 10) {
+      let letter = expr.substring(i, i + 10);
+      if(letter === '**********') {
+        sentence = sentence + ' ';
+      } else{
+        let newLetter = letter.replaceAll('00','');
+        let newLetter2 = newLetter.replaceAll('10','.');
+        let newLetter3 = newLetter2.replaceAll('11','-');
+        let world = MORSE_TABLE[newLetter3];
+        sentence = sentence + world;
+      }
+    }
+    return sentence;
+  }
 
 module.exports = {
     decode
